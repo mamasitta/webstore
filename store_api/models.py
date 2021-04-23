4d26ca2cb88f863c3ab6e895883b614f4c7f1ff2
Victoria <vikiber2013@gmail.com>
2021-04-23 11:00:29 +0300

    create custom user model

--- store_api/models.py
+++ store_api/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
