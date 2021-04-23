8ed03656071e56245fbdbe38a944c65a162623be
Victoria <vikiber2013@gmail.com>
2021-04-23 20:19:12 +0300

    Order and order items table

--- store_api/admin.py
+++ store_api/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
 
-from store_api.models import User, Product, ProductPicture
+from store_api.models import User, Product, ProductPicture, OrderItem, Order
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Product)
 admin.site.register(ProductPicture)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- store_api/models.py
+++ store_api/models.py
@@ -18,19 +18,19 @@ class Product(models.Model):
     price = models.IntegerField()
 
 
-# class Order(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
-#     date_ordered = models.DateTimeField()
-#     date_complete = models.DateTimeField(null=True)
-#     address = models.TextField()
-#     phone = models.CharField(max_length=20)
-#     complete = models.BooleanField(default=False)
-#
-#
-# class OrderItem(models.Model):
-#     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_item')
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_item')
-#     amount = models.IntegerField()
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer')
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    date_complete = models.DateTimeField(null=True, blank=True)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+    complete = models.BooleanField(default=False)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_item')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_item', verbose_name="prod")
+    amount = models.IntegerField()
 
 
 class ProductPicture(models.Model):
